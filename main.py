99db18a81ab52067fbc5cd11255c4ee739eef036
Dominic Canare <dom@makeict.org>
2016-12-08 21:40:20 -0600

    Basic event creation in WildApricot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
 from PySide import QtCore, QtGui
 
 import plugins
--- plugins/WildApricot/__init__.py
+++ plugins/WildApricot/__init__.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import ui
+from PySide import QtCore
 
-class WildApricotPlugin:
+from .WildApricotAPI import WaApiClient
+
+from config import settings
+
+class WildApricotPlugin(QtCore.QObject):
 	def __init__(self):
 		self.options = [
 			{
 				'name': 'API Key',
+				'type': 'text',
+			},{
+				'name': 'Client ID',
+				'type': 'text',
+			},{
+				'name': 'Client secret',
 				'type': 'text',
 			},{
 				'name': 'Customer ID',
@@ -17,8 +28,41 @@ class WildApricotPlugin:
 		ui.addTarget('WildApricot', self.createEvent)
 		
 	def createEvent(self, event):
-		print(event)
-		pass
+		api = WaApiClient()
+		api.authenticate_with_apikey(self._getSetting('API Key'))
 
+		# @todo: add authorizations to description
+		if settings.value('timezone') is not None and settings.value('timezone') != '':
+			timezoneOffset = settings.value('timezone').split(' UTC')[1]
+		else:
+			timezoneOffset = ''
+
+		
+		eventData = {
+			"Name": 'TEST: ' + event['title'],
+			"StartDate": event['startTime'].toString(QtCore.Qt.ISODate) + timezoneOffset,
+			"EndDate": event['stopTime'].toString(QtCore.Qt.ISODate) + timezoneOffset,
+			"Location": event['location'],
+			"RegistrationsLimit": event['registrationLimit'],
+			"RegistrationEnabled": False,
+			"StartTimeSpecified": True,
+			"EndTimeSpecified": True,
+			"Details": {
+				"DescriptionHtml": event['description'],
+				"AccessControl": { "AccessLevel": "Public" },
+				"GuestRegistrationSettings": { "CreateContactMode": "CreateContactForAllGuests" },
+				"PaymentMethod": "OnlineOnly",
+				"SendEmailCopy": False,
+				"WaitListBehaviour": "Disabled",
+			},
+		}
+		
+		eventID = api.execute_request('Events', eventData)
+		print(eventID)
+
+
+	def _getSetting(self, setting):
+		return settings.value('plugin-WildApricot/%s' % setting)
+	
 def load():
 	return WildApricotPlugin()
