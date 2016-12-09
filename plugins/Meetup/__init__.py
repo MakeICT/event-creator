461ec473d398798f3b24cfcff33e415bf7d71044
Dominic Canare <dom@makeict.org>
2016-12-09 03:16:42 -0600

    Basic meetup support

--- plugins/Meetup/__init__.py
+++ plugins/Meetup/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import meetup.api
 import ui
 
 from ..Plugin import Plugin
@@ -12,14 +13,46 @@ class MeetupPlugin(Plugin):
 			{
 				'name': 'API Key',
 				'type': 'text',
+			},{
+				'name': 'Group name',
+				'type': 'text',
+			},{
+				'name': 'Venue ID',
+				'type': 'text',
+			},{
+				'name': 'Official registration site',
+				'type': 'yesno',
 			}
 		]
 
 		ui.addTarget('Meetup', self.createEvent)
 		
 	def createEvent(self, event):
-		print(event)
-		pass
+		api = meetup.api.Client(self.getSetting('API Key'))
+		group = api.GetGroup({'urlname': self.getSetting('Group name')})
+		
+		if event['registrationURL'] != '':
+			description = 'To register for this event, please visit <a href="%s">%s</a>' % (event['registrationURL'], event['registrationURL'])
+			description += '\n\n'
+		else:
+			description = ''
+			
+		description += event['description']
+		
+		meetupEvent = api.CreateEvent({
+			'group_id': group.id,
+			'name': event['title'],
+			'description': description,
+			'time': event['startTime'].toTime_t() * 1000,
+			'duration': event['startTime'].msecsTo(event['stopTime']),
+			'venue_id': self.getSetting('Venue ID'),
+			'publish_status': 'draft',
+		})
+		
+		if config.checkBool(self.getSetting('Official registration site')):
+			event['registrationURL'] = meetupEvent.event_url
 
+		return event
+		
 def load():
 	return MeetupPlugin()
