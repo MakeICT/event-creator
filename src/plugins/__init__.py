cc01575966acece6cfcfb77fcaa0b6e69f0a02ed
Christian Kindel <iceman81292@gmail.com>
2019-07-31 10:19:00 -0500

    Revert "Ported Christians updates to fix the posting of events." and "Added support to convert the event date/time to GMT"
    This reverts commit 9e9fbc5ab6f6092ed5f98eeb6ca75a827132bf25.
    This reverts commit 59572069b94e38e931675240a4fe51652a7efcfa.

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -7,14 +7,6 @@ class Plugin(QtCore.QObject):
     def __init__(self, name):
         super().__init__()
         self.name = name
-
-    def getGeneralSetting(self, setting, default=''):
-        value = settings.get('General', setting)
-        #value = settings['plugin-%s/%s' % (self.name, setting)]
-        if value == '':
-            value = default
-            
-        return value
         
     def getSetting(self, setting, default=''):
         value = settings.get('plugin-' + self.name, setting)
