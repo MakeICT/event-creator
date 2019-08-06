0da40f714c09be6b7f30ceea7847bb4e3fbf1cff
Christian Kindel <iceman81292@gmail.com>
2019-08-06 10:42:32 -0500

    add page to display upcoming events

--- src/main.py
+++ src/main.py
@@ -207,6 +207,40 @@ def createClass(template):
                            
             # waplugin.createEvent(event)
             return redirect(url_for('home'))
+
+@app.route('/events', methods=['GET'])
+def upcoming_events():
+    upcoming_events = Event.query.all()
+    upcoming_events = sorted(upcoming_events, key=lambda event: event.start_date)
+    event_list = []
+    for event in upcoming_events:
+        if event.start_date >= datetime.datetime.today().date():
+        # if not event['AccessLevel'] == 'AdminOnly':
+            if event.registration_limit:
+                # print(event['RegistrationsLimit'], event['ConfirmedRegistrationsCount'])
+                spots_available = event.registration_limit
+                spots = None
+                if spots_available > 0:
+                    spots = str(spots_available) + 'Register'
+                else:
+                    spots = 'FULL'
+            # start_date = WA_API.WADateToDateTime(event['StartDate'])
+            start_date = event.start_date
+            if 'test' not in event.title.lower():
+                event_list.append({
+                                    "Id": event.id,
+                                    "Name":event.title,
+                                    "Date": start_date.strftime('%b %d %Y'),
+                                    "Time": start_date.strftime('%I:%M %p'),
+                                    "Register":"http://makeict.wildapricot.org/event-" + str(event.id),
+                                  })
+
+            #       str(event['Id']))
+            # print(start_date.strftime('%b %d') + ' | ' + start_date.strftime('%I:%M %p') + 
+            #       ' | ' + event['Name'] + ' | ' + '<a href="http://makeict.wildapricot.org/event-' + 
+            #       str(event['Id']) + '" target="_blank">Register</a><br />')
+
+    return render_template('events.html', events=event_list)
     
 
     
