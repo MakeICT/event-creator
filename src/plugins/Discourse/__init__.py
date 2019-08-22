75ee1280dc778dd437e001fe90fcef90540d6495
Christian Kindel <iceman81292@gmail.com>
2019-08-22 10:33:27 -0500

    style: clean up Discourse plugin

--- src/plugins/Discourse/__init__.py
+++ src/plugins/Discourse/__init__.py
@@ -12,71 +12,82 @@ from plugins import Plugin
 from config import settings
 import config
 
+
 class DiscoursePlugin(Plugin):
-	def __init__(self):
-		super().__init__('Discourse')
-		
-		self.options = [
-			{
-				'name': 'Website',
-				'type': 'text',
-			},{
-				'name': 'Username',
-				'type': 'text',
-			},{
-				'name': 'API Key',
-				'type': 'text',
-			},{
-				'name': 'Category ID',
-				'type': 'text',
-			},{
-				'name': 'Date/Time format',
-				'type': 'text',
-			},
-		]
+    def __init__(self):
+        super().__init__('Discourse')
 
-		# ui.addTarget(self.name, self, self.createForumPost)
-		# ui.addPopulationType('Members')
-	
-	def createForumPost(self, event):
-		logging.debug('Discourse')
+        self.options = [
+            {
+                'name': 'Website',
+                'type': 'text',
+            }, {
+                'name': 'Username',
+                'type': 'text',
+            }, {
+                'name': 'API Key',
+                'type': 'text',
+            }, {
+                'name': 'Category ID',
+                'type': 'text',
+            }, {
+                'name': 'Date/Time format',
+                'type': 'text',
+            },
+        ]
 
-		dateTimeFormat = self.getSetting('Date/Time format', 'yyyy MMM dd - h:mm ap')
+        # ui.addTarget(self.name, self, self.createForumPost)
+        # ui.addPopulationType('Members')
 
-		description = "**Starting:** " + event['startTime'].toString(dateTimeFormat)
-		description += '\n' + "**Ending:** " + event['stopTime'].toString(dateTimeFormat)
-		description += '\n' + "**Instructor:** " + ' '.join(event['instructorDescription'].split(' ')[1:])
-		
-		if event['registrationURL'] is not None and event['registrationURL'] != '':
-			description += '\n' + "**Register:** " + '[' + event['registrationURL'] + '](' + event['registrationURL'] + ')'
+    def createForumPost(self, event):
+        logging.debug('Discourse')
 
-		description += '\n' + "**Price:** " + event['priceDescription'][28:]
+        dateTimeFormat = self.getSetting(
+                                'Date/Time format', 'yyyy MMM dd - h:mm ap')
 
-		if event['ageDescription']:
-			description += '\n' + "**Ages:** " + ' '.join(event['ageDescription'].split(' ')[1:])
+        description = "**Starting:** " \
+            + event['startTime'].toString(dateTimeFormat)
+        description += '\n' + "**Ending:** " \
+            + event['stopTime'].toString(dateTimeFormat)
+        description += '\n' + "**Instructor:** " \
+            + ' '.join(event['instructorDescription'].split(' ')[1:])
 
-		if event['authorizationDescription']:
-			description += '\n' + "**Required Authorizations:** " + ', '.join(event['authorizationDescription'].split(' ')[2:])
+        if event['registrationURL'] and event['registrationURL'] != '':
+            description += '\n' + "**Register:** " + '[' \
+                + event['registrationURL'] + '](' + event['registrationURL'] + ')'
 
-		description += '\n\n' + event['description'] 
+        description += '\n' + "**Price:** " + event['priceDescription'][28:]
 
-		self.checkForInterruption()
+        if event['ageDescription']:
+            description += '\n' + "**Ages:** " \
+                + ' '.join(event['ageDescription'].split(' ')[1:])
 
-		logging.debug('Connecting to API')
-		print('username:',self.getSetting('Username'))
-		print('api key:',self.getSetting('API Key'))
-		
-		discourse_api = DiscourseClient(
-			self.getSetting('Website'),
-			api_username=self.getSetting('Username'),
-			api_key=self.getSetting('API Key'))
+        if event['authorizationDescription']:
+            description += '\n' + "**Required Authorizations:** " \
+                + ', '.join(event['authorizationDescription'].split(' ')[2:])
 
-		
-		self.checkForInterruption()
-		logging.debug('Creating post')
-		title = 'Event notice: ' + event['title'] + ' (' + event['startTime'].toString(dateTimeFormat) + ')'
-		discourse_api.create_post(content=description, category_id=int(self.getSetting('Category ID')), topic_id=None, title=title)
-		return 
+        description += '\n\n' + event['description']
+
+        self.checkForInterruption()
+
+        logging.debug('Connecting to API')
+        print('username:', self.getSetting('Username'))
+        print('api key:', self.getSetting('API Key'))
+
+        discourse_api = DiscourseClient(
+            self.getSetting('Website'),
+            api_username=self.getSetting('Username'),
+            api_key=self.getSetting('API Key'))
+
+        self.checkForInterruption()
+        logging.debug('Creating post')
+        title = 'Event notice: ' + event['title'] + ' (' \
+            + event['startTime'].toString(dateTimeFormat) + ')'
+        discourse_api.create_post(content=description,
+                                  category_id=int(self.getSetting('Category ID')),
+                                  topic_id=None, title=title)
+        return
+
 
 def load():
-	return DiscoursePlugin()
+    return DiscoursePlugin()
