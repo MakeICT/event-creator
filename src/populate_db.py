b374155069678c7c9d4c60110a2e1c543aeff1c3
Christian Kindel <iceman81292@gmail.com>
2019-08-23 09:48:28 -0500

    style: clean up formatting

--- src/populate_db.py
+++ src/populate_db.py
@@ -1,20 +1,22 @@
 import os
 from main import db, Event, Authorization
 
+
 def populate_auths():
-    auth_map = {'Woodshop':416232,
-            'Metalshop':416231,
-            'Forge':420386,
-            'LaserCutter':416230,
-            'Mig welding':420387, 
-            'Tig welding':420388, 
-            'Stick welding':420389, 
-            'Manual mill':420390,            
-            'Plasma':420391, 
-            'Metal lathes':420392, 
-            'CNC Plasma':420393, 
-            'Intro Tormach':420394, 
-            'Full Tormach':420395}
+    auth_map = {'Woodshop': 416232,
+                'Metalshop': 416231,
+                'Forge': 420386,
+                'LaserCutter': 416230,
+                'Mig welding': 420387,
+                'Tig welding': 420388,
+                'Stick welding': 420389,
+                'Manual mill': 420390,
+                'Plasma': 420391,
+                'Metal lathes': 420392,
+                'CNC Plasma': 420393,
+                'Intro Tormach': 420394,
+                'Full Tormach': 420395}
+
     for auth in auth_map:
         print(auth)
         a = Authorization(name=auth, wa_group_id=auth_map[auth])
