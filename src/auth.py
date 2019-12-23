15f4a4dd9eb94de0c8e0c9850d266ed97269f776
Christian Kindel <iceman81292@gmail.com>
2019-12-23 13:53:48 -0600

    clean up auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,7 +1,7 @@
-from flask import render_template, url_for, redirect, request, session, Flask, Response, flash
-from flask_login import login_user, logout_user, current_user, login_required, LoginManager, UserMixin
+from flask import render_template, url_for, redirect, request, flash
+from flask_login import login_user, logout_user, current_user, login_required,\
+                        LoginManager, UserMixin
 
-from plugins import WildApricot
 from main import app, loadedPlugins
 
 login_manager = LoginManager()
@@ -16,66 +16,62 @@ class User(UserMixin):
         self.groups = []
         self.id = 0
 
-        
+
 @login_manager.user_loader
 def load_user(user_id):
     return User(user_id)
+
 
 @login_manager.unauthorized_handler
 def unauthorized_callback():
     return redirect('/login')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    User login page
+    """
     # redirect to home page if a user is already logged in
     if current_user.is_authenticated:
         return redirect(url_for('home'))
 
     error = None
-    if request.method == 'POST':        
+    if request.method == 'POST':
         user = validateWAUser(request.form['username'], request.form['password'])
-        if user is None:
+        if not user:
             flash('Please check your login details and try again.', 'danger')
         else:
             login_user(user)
             return redirect(url_for('home'))
-        
-    
-            
+
     return render_template('login.html', error=error)
+
 
 @app.route("/logout", methods=["GET"])
 @login_required
 def logout():
-    """Logout the current user."""
-    user = current_user
-    if user is not None:
-        user.authenticated = False
+    """
+    Logout the current user.
+    """
+    if current_user.is_authenticated:
         logout_user()
-        
-    return redirect("login")
+
+    return redirect(url_for('home'))
+
 
 def validateWAUser(username, password):
+    """
+    Authenticate username, password and group membership against Wild Apricot
+    """
     waplugin = loadedPlugins['WildApricot']
-#    approved = waplugin.isUserValid(username, password)
-# 
-#    if not approved:
-#        return None
-    
     user = waplugin.loadUser(username, password)
-    
-    if user is None:
-        return None
-    
-    try:
-        groups = user.groups
-    
+
+    if user:
         authorizedGroups = waplugin.getSetting('Authorized Groups').split(',')
-        
+
         for group in authorizedGroups:
-            if groups.count(group) > 0:
+            if user.groups.count(group) > 0:
                 return user
-    except:
-        return None
-    
+
     return None
